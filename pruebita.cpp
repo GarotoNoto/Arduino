d942bcd0a87f269cbb1060d75befacab049e5dad
GarotoNoto <josuegalindo014@gmail.com>
2023-12-10 16:22:59 -0600

    primer commit

---
+++ pruebita.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+//#include "./libraries/Chrono/Chrono.h"
+//#include "./libraries/Time/TimeLib.h"
+
+int main(){
+    /*auto tiempo = std::chrono::system_clock::now();
+
+    std::cout << "El tiempo es el siguiente: " << tiempo << std::endl; */
+
+    int num;
+
+    // Output
+    std::cout << "Enter a number: ";
+
+    // Input
+    std::cin >> num;
+
+    // Display the entered number
+    std::cout << "You entered: " << num << std::endl;
+
+    return 0;
+}
